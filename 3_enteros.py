6fd323edbf65207f250c1865eece653ca32806f8
PhilippeParra <jonathan2224509@correo.uis.edu.co>
2022-05-03 14:55:28 -0500

    Desarrollo ejercicio No. 1 3/05/2022. PhilippeParra

---
+++ 3_enteros.py
@@ -0,0 +1,16 @@
+"""Ejercicio No. 1 3/05/2022"""
+
+
+print("---------------------------------")
+print("-----------3 ENTEROS-------------")
+print("---------------------------------")
+
+
+#input
+
+X = int(input("Digite el Primer entero: "))
+Y = int(input("Digite el Segundo entero: "))
+Z = int(input("Digite el Tercer entero: "))
+
+if X + Y == Z:
+    print("La suma de los primeros dos enteros \n" + str(X) + "\n" + str(Y) + "\nes igual al tercero " + str(Z))
---
+++ README.md
@@ -0,0 +1,6 @@
+# Ejercicio No. 1 3/05/2022
+
+## Dados 3 numeros enteros identificar si la suma de
+## los dos primeros es igual al 3
+
+![Diagrama de flujo](3_enteros.png "Diagrama de flujo")
